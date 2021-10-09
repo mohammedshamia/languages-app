d86ebfdde95e4bbfcca60d41321d720df639df4e
mohammedshamia <mohdkarera@gmail.com>
2021-10-09 12:00:41 +0300

    I18next

--- src/App.js
+++ src/App.js
@@ -1,93 +1,37 @@
 import './App.css'
-import {useState} from "react";
-
-const language={
-    Ar:{
-        header:{
-            Home:"الرسية",
-            About:"عنا",
-            "Contact":"تاصل",
-        },
-        Cites:{
-            title:"المدن",
-            Gaza:"غزة",
-            Nablus:"نابلس",
-            Rafah:"رفح",
-            London:"لندن",
-            Paris:"باريس",
-            Hebron:"خليل",
-            Jerusalem:"قدس",
-        }
-    },
-    En:{
-        header:{
-            Home:"Home",
-            About:"About",
-            "Contact":"Contact us",
-        },
-        Cites:{
-            title:"Cites",
-            Gaza:"Gaza",
-            Nablus:"Nablus",
-            Rafah:"Rafah",
-            London:"London",
-            Paris:"Paris",
-            Hebron:"Hebron",
-            Jerusalem:"Jerusalem",
-        }
-    },
-    Fr:{
-        header:{
-            Home:"Home Fr",
-            About:"About Fr",
-            "Contact":"Contact us Fr",
-        },
-        Cites:{
-            title:"Cites Fr",
-            Gaza:"Gaza Fr",
-            Nablus:"Nablus Fr",
-            Rafah:"Rafah Fr",
-            London:"London Fr",
-            Paris:"Paris Fr",
-            Hebron:"Hebron Fr",
-            Jerusalem:"Jerusalem Fr",
-        }
-    }
-}
+import changeLanguage from "./lang/changeLang";
+import {useTranslation} from "react-i18next";
 
 function App() {
-    const lang=localStorage.getItem("lang")
-    const [state, setState]=useState(lang || "Ar")
+    const {i18n, t}=useTranslation()
 
     const setLang=(lang)=>{
-        setState(lang)
-        localStorage.setItem("lang", lang)
+        changeLanguage(lang, i18n)
     }
 
-    const translatedObj=language[state]
   return (
-    <div className={`App ${state==="Ar"&&"rtlDr"}`}>
+    <div className={`App ${i18n.language==="Ar"&&"rtlDr"}`}>
       <header className="App-header fl fdr">
             <a className={"a"}>
-                {translatedObj.header.Home}
+                {t('header.Home')}
             </a>
           <a className={"a"}>
-              {translatedObj.header.About}
+              {t("header.About")}
             </a>
           <a className={"a"}>
-              {translatedObj.header['Contact']}
+              {t("header.Contact")}
             </a>
       </header>
 
         <div className={"mt fl fdc"}>
-            <h1 className={"ml"}>{translatedObj.Cites.title}</h1>
-            <h2 className={"ml"}>{translatedObj.Cites.Gaza}</h2>
-            <h2 className={"ml"}>{translatedObj.Cites.Nablus}</h2>
-            <h2 className={"ml"}>{translatedObj.Cites.Rafah}</h2>
-            <h2 className={"ml"}>{translatedObj.Cites.London}</h2>
-            <h2 className={"ml"}>{translatedObj.Cites.Paris}</h2>
-            <h2 className={"ml"}>{translatedObj.Cites.Hebron}</h2>
-            <h2 className={"ml"}>{translatedObj.Cites.Jerusalem}</h2>
+            <h1 className={"ml"}>{t("Cites.title")}</h1>
+            <h2 className={"ml"}>{t("Cites.Gaza")}</h2>
+            <h2 className={"ml"}>{t("Cites.Nablus")}</h2>
+            <h2 className={"ml"}>{t("Cites.Rafah")}</h2>
+            <h2 className={"ml"}>{t("Cites.London")}</h2>
+            <h2 className={"ml"}>{t("Cites.Paris")}</h2>
+            <h2 className={"ml"}>{t("Cites.Hebron")}</h2>
+            <h2 className={"ml"}>{t("Cites.Jerusalem")}</h2>
         </div>
 
         <div className={"mt fl"}>
@@ -95,7 +39,7 @@ function App() {
             <button onClick={()=>setLang("En")}>En</button>
             <button onClick={()=>setLang("Fr")}>Fr</button>
         </div>
-        {state}
+        {i18n.language}
     </div>
   );
 }
